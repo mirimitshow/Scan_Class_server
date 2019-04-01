f66d20722fa482514431211ca66e44f6b9cd27e5
wisemuji <wisemuji@gmail.com>
2019-04-01 13:02:33 +0900

    get group by token

---
+++ routes/group/getGroup.js
@@ -0,0 +1,7 @@
+module.exports = (app, Users, Groups) => {
+    app.post('/getGroup', async(req, res) => {
+        var result = await Groups.find({ token: req.body.token });
+        if (!result) res.status(400).json({ message: 'No Group Found' });
+        else res.status(200).json(result);
+    })
+}

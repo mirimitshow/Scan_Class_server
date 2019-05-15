29afbdfdd1dce137d40343942fa6223913226c04
wisemuji <wisemuji@gmail.com>
2019-05-16 02:02:49 +0900

    POST /getUserGroups User가 속한 모든 그룹 조회

--- routes/group/getGroup.js
+++ routes/group/getGroup.js
@@ -4,4 +4,21 @@ module.exports = (app, Users, Groups) => {
         if (!result) res.status(400).json({ message: 'No Group Found' });
         else res.status(200).json(result);
     })
+    app.get('/getUserGroups/:email', async(req, res) => {
+        let email = req.params.email;  
+        let groups = [];   
+        let cnt = 0;
+        await Users.findOne({ email: email }, async (err, rawContent)=>{
+            if(err) res.status(400).json({ message: err });
+            else if(!rawContent) res.status(400).json({ message: 'No User Found' });
+            await rawContent.group.forEach(async element => {
+                let result = await Groups.find({ token: element.token });
+                groups.push(result);
+                cnt++; 
+                if (cnt === rawContent.group.length) {
+                    res.status(200).json(groups);
+                }
+            });
+        });
+    })
 }

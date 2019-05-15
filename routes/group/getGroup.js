2d6ca7e80c6370b27791b6aea2b8442289773693
wisemuji <wisemuji@gmail.com>
2019-05-16 00:10:43 +0900

    getGroup 메소드 POST형태로 변경

--- routes/group/getGroup.js
+++ routes/group/getGroup.js
@@ -1,5 +1,5 @@
 module.exports = (app, Users, Groups) => {
-    app.post('/getGroup', async(req, res) => {
+    app.get('/getGroup', async(req, res) => {
         var result = await Groups.find({ token: req.body.token });
         if (!result) res.status(400).json({ message: 'No Group Found' });
         else res.status(200).json(result);

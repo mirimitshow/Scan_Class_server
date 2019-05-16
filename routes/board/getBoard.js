22f79af8c1df43821963163523a86dd23c1add49
wisemuji <wisemuji@gmail.com>
2019-05-16 15:10:15 +0900

    GET /getGroupBoards/:token 그룹의 모든 board 조회

--- routes/board/getBoard.js
+++ routes/board/getBoard.js
@@ -4,4 +4,12 @@ module.exports = (app, Boards) => {
         if (!result) res.status(400).json({ message: 'No Board Found' });
         else res.status(200).json(result);
     })
+    .get('/getGroupBoards/:token', async(req, res) => {
+        let token = req.params.token;  
+        await Boards.find({ group_token: token }, async (err, rawContent)=>{
+            if(err) res.status(400).json({ message: err });
+            else if(!rawContent) res.status(204).json({ message: 'No Board Found' });
+            res.status(200).json({boards: rawContent});
+        });
+    });
 }

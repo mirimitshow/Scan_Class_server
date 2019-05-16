89657aecbaef8ad67e1bb7d887411f8a7e417b2d
wisemuji <wisemuji@gmail.com>
2019-05-16 21:02:58 +0900

    status code 204 -> 209

--- routes/scan/getScan.js
+++ routes/scan/getScan.js
@@ -1,7 +1,7 @@
 module.exports = (app, Scans) => {
     app.get('/getScans/:email/:cartegory', async(req, res) => {
         var result = await Scans.find({ cartegory: req.params.cartegory, email: req.params.email });
-        if (!result) res.status(204).json({ message: 'No Scans Found' });
+        if (!result) res.status(209).json({ message: 'No Scans Found' });
         else res.status(200).json({scans: result});
     })
 }

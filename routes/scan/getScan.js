8c5dfd97ecb5fac3f9c06bfd9d49044220154c2e
wisemuji <wisemuji@gmail.com>
2019-05-16 21:00:07 +0900

    /getScan => /getScans

--- routes/scan/getScan.js
+++ routes/scan/getScan.js
@@ -1,5 +1,5 @@
 module.exports = (app, Scans) => {
-    app.get('/getScan/:email/:cartegory', async(req, res) => {
+    app.get('/getScans/:email/:cartegory', async(req, res) => {
         var result = await Scans.find({ cartegory: req.params.cartegory, email: req.params.email });
         if (!result) res.status(204).json({ message: 'No Scans Found' });
         else res.status(200).json({scans: result});

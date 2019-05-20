994d182a46c67fcb5f154e73905ab70798d60d22
wisemuji <wisemuji@gmail.com>
2019-05-20 10:10:43 +0900

    getBoard 배열이 아닌 객체 반환

--- routes/board/getBoard.js
+++ routes/board/getBoard.js
@@ -1,6 +1,6 @@
 module.exports = (app, Boards) => {
     app.get('/getBoard/:token', async(req, res) => {
-        var result = await Boards.find({ token: req.params.token });
+        var result = await Boards.findOne({ token: req.params.token });
         if (!result) res.status(400).json({ message: 'No Board Found' });
         else res.status(200).json(result);
     })

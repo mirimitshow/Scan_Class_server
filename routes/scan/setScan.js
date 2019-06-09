6fbc81c3459deae727d06175d65ebe15ac1e1a31
wisemuji <wisemuji@gmail.com>
2019-06-09 16:25:55 +0900

    카테고리 중복 추가 수정

--- routes/scan/setScan.js
+++ routes/scan/setScan.js
@@ -19,9 +19,15 @@ module.exports = (app, Users, Scans) => {
         let scan = new Scans(req.body);
         scan.id = req.file.filename;
         scan.url = URL + req.file.filename;
-        await Users.findOne({email: req.body.email}, (err, rawContent)=>{
+        await Users.findOne({email: req.body.email}, async (err, rawContent)=>{
             if(err) res.status(400).json({message: "error!"});
-            rawContent.cartegory.unshift({ name: req.body.cartegory });
+
+            var found = await rawContent.cartegory.find(function(element) {
+                console.log(element)
+                return element.name === req.body.cartegory;
+            });
+            if(found == null) rawContent.cartegory.unshift({ name: req.body.cartegory });
+
             rawContent.save(async (err)=>{
                 var result = await scan.save((err)=>{
                     if(err) res.status(400).json({message: "error!"});

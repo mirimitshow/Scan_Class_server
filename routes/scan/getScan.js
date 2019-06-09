6b28181bb641494fbd13090e0631233b5347248a
wisemuji <wisemuji@gmail.com>
2019-06-09 16:46:26 +0900

    getScans의 bracket 제거

--- routes/scan/getScan.js
+++ routes/scan/getScan.js
@@ -2,7 +2,7 @@ module.exports = (app, Scans) => {
     app.get('/getScans/:email/:cartegory', async(req, res) => {
         var result = await Scans.find({ cartegory: req.params.cartegory, email: req.params.email });
         if (!result) res.status(209).json({ message: 'No Scans Found' });
-        else res.status(200).json({scans: result});
+        else res.status(200).json(result);
     })
     .post('/aaScan', async(req, res) => {
         var result = await Scans.find();
